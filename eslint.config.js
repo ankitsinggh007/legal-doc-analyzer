542e942188014344d1c87d38b110560b81964cea
Ankit Singh <as3824115@gmail.com>
2025-10-08 11:56:04 +0530

    chore: initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-config-prettier";
+import babelParser from "@babel/eslint-parser";
+
+export default [
+  {
+    files: ["**/*.{js,jsx}"],
+    ignores: ["dist/**", "node_modules/**", ".vite/**"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: babelParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        requireConfigFile: false,
+        babelOptions: {
+          plugins: ["@babel/plugin-syntax-jsx"],
+        },
+      },
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        navigator: "readonly",
+      },
+    },
+
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...prettier.rules,
+      "react/react-in-jsx-scope": "off",
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // we’ll toggle dark mode later
+  plugins: [],
+};
